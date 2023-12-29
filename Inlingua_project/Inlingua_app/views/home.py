77e57565162502f8eb80b90f32075e0786da8833
vickz003 <muthuvicky7777@gmail.com>
2023-12-29 12:23:58 +0530

    updated by 29-12-2023 | 12:33

--- Inlingua_project/Inlingua_app/views/home.py
+++ Inlingua_project/Inlingua_app/views/home.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from Inlingua_app.models import User, Courses, Level, Languages, TrainingStaff, TrainerQualifications, TrainingBatches, StudentDetails
+from Inlingua_app.models import User, TrainingStaff, TrainerQualifications, TrainingBatches, StudentDetails
 
 def home(request):
     if request.user.is_authenticated:
@@ -12,15 +12,26 @@ def home(request):
                 return render(request, 'inlingua/index.html')
                 # Handle superuser logic here (if needed)
             else:
-                training_staff = TrainingStaff.objects.get(LoginId=user)
-                trainer_qualifications = TrainerQualifications.objects.get(TrainerId=training_staff)
-                training_batches = TrainingBatches.objects.filter(TrainerId=training_staff)
+                trainer_details = TrainingStaff.objects.get(LoginId=user)
+                training_batches = TrainingBatches.objects.filter(TrainerId=trainer_details.ID)
+                trainer_qualifications = TrainerQualifications.objects.get(ID=trainer_details.ID)
+
+                start_times = training_batches.filter(TrainerId=trainer_details.ID).values_list('StartTime', flat=True)
+                if start_times:
+                    min_start_time = min(start_times)
+                    today_first_batch = training_batches.filter(StartTime=min_start_time).first()
+
+                else:
+                    print("No StartTime available.")
+                    today_first_batch = None
                 return render(request, 'inlingua/index.html', {
                     'user': user,
-                    'training_staff': training_staff,
-                    'trainer_qualifications': trainer_qualifications,
+                    'trainer_details': trainer_details,
                     'training_batches': training_batches,
+                    'trainer_qualifications': trainer_qualifications,
+                    'today_first_batch':today_first_batch,
                 })
+
         # Stident details
         elif user.is_active:
             student_details = StudentDetails.objects.get(StudentID=user)
